4da3d044562451c65e29b20497a45a4047957355
Beximus89 <cholponov.997@gmail.com>
2022-05-05 20:07:38 +0300

    Добавлены: упрощенное обращение к выходам; мигание двумя разными битами порта с разной частотой (пока с целой кратностью)

--- src/main.c
+++ src/main.c
@@ -1,4 +1,9 @@
 #include "stm32f051x8.h"
+#define PC8 GPIO_ODR_8
+#define PC9 GPIO_ODR_9
+int DIV;
+
+
 
 
 uint8_t i=0;
@@ -8,9 +13,14 @@ void TIM2_IRQHandler(void)
 {
   TIM2->SR &= ~TIM_SR_UIF; //Clean UIF Flag
 
-    GPIOC->ODR ^= GPIO_ODR_9;   // Set PC8 bit
-    GPIOC->ODR ^= GPIO_ODR_8;   // Reset PC9 bit
+    GPIOC->ODR ^= PC9;   // Set PC8 bit
+    
+    if (DIV == 2){
 
+      GPIOC->ODR ^= PC8;   // Reset PC9 bit
+      DIV =0;
+    }
+    DIV++;
 }
 
 
@@ -23,13 +33,14 @@ int main(void)
   GPIOC->MODER &= ~(GPIO_MODER_MODER8 | GPIO_MODER_MODER9);
   // Configure PC.8 and PC.9 as Push Pull output at max 10Mhz
   GPIOC->MODER |= GPIO_MODER_MODER8_0 | GPIO_MODER_MODER9_0;
-  TIM2->PSC = SystemCoreClock / 1000 - 1; // 1000 tick/sec
+  TIM2->PSC = SystemCoreClock / 5000 - 1; // 1000 tick/sec
   TIM2->ARR = 1000;  // 1 Interrupt/sec (1000/100)
   TIM2->DIER |= TIM_DIER_UIE; // Enable tim2 interrupt
   TIM2->CR1 |= TIM_CR1_CEN;   // Start count
   NVIC_EnableIRQ(TIM2_IRQn);  // Enable IRQ
-  GPIOC -> BSRR |= GPIO_BSRR_BS_8;
-  
+  //GPIOC -> BSRR |= GPIO_BSRR_BS_8;
+
+;
 }
  
 
